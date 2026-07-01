c0410c7a87274895f1cae34c35a90a7e3b774534
carranzatomas73-source <carranzatomas73@gmail.com>
2026-07-01 09:59:14 -0300

    Medicion de tension y puerto serial

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,33 @@
 #include <Arduino.h>
 
+float factorDivision = 1.0;
 
 void setup() {
- pinMode(2, OUTPUT); // Configura el pin del LED como salida
+  Serial.begin(115200);
+  pinMode(34, INPUT);
 }
 
 void loop() {
-  digitalWrite(2, HIGH); // Enciende el LED
-  delay(500); // Espera 1 segundo
-  digitalWrite(2, LOW); // Apaga el LED
-  delay(500); // Espera 1 segundo
-  
+  long suma = 0;
+  const int muestras = 20;
+
+  for (int i = 0; i < muestras; i++) {
+    suma += analogRead(34);
+    delay(2);
+  }
+
+  int adcPromedio = suma / muestras;
+
+  float voltajeReal = (adcPromedio * 3.3 / 4095.0) * factorDivision;
+
+  Serial.print("ADC Promedio: ");
+  Serial.println(adcPromedio);
+
+  Serial.print("Voltaje: ");
+  Serial.print(voltajeReal, 3);
+  Serial.println(" V");
+  Serial.print("ADC Promedio: ");
+  Serial.println(adcPromedio);
+
+  delay(100);
 }
